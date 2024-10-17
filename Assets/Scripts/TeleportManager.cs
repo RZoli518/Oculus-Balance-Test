d3caad4b84343ecbe07c46326055fbf506ce47d3
Devley0 <getkhun@gmail.com>
2024-10-17 11:42:33 +0200

    TeleportManager.cs teleports the user to three different positions. Updated UnityScene: three platforms (ground level, 15meter, 30 meter), shrunk the city, to approximate the height. Imported MetaXR asset, to use controller input.

--- Assets/Scripts/TeleportManager.cs
+++ Assets/Scripts/TeleportManager.cs
@@ -1,24 +1,28 @@
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using static OVRInput;
 
 public class TeleportManager : MonoBehaviour
 {
     public Transform[] teleportPositions;
     private int positionIdx = 0;
-    private int maxInterval = 1;
+    private double maxInterval = 0.4; //how many seconds before an input.
     private float currentInterval = 0;
+    void Start()
+    {
+        TeleportNextPosition();
+    }
     void Update()
     {
-        Debug.Log(IsButtonPressed());
         if (currentInterval <= 0)
         {
             if (IsButtonPressed())
             {
+                positionIdx = IncrementTeleportPosition(positionIdx);
                 TeleportNextPosition();
+                currentInterval = (float)maxInterval;
             }
-            currentInterval = maxInterval;
         }
         else
         {
@@ -26,14 +30,14 @@ public class TeleportManager : MonoBehaviour
         }
     }
 
-    private void TeleportNextPosition() => transform.position = teleportPositions[IncrementTeleportPosition(positionIdx)].position;
+    private void TeleportNextPosition() => gameObject.transform.SetPositionAndRotation(teleportPositions[positionIdx].position, gameObject.transform.rotation);
 
-    private bool IsButtonPressed() => OVRInput.Get(Button.One) || OVRInput.Get(Button.Two);
+    private bool IsButtonPressed() => OVRInput.Get(Button.One) || OVRInput.Get(Button.Three);
 
     private int IncrementTeleportPosition(int idx)
     {
         idx++;
-        if (idx > teleportPositions.Length) return 0;
+        if (idx == teleportPositions.Length) return 0;
         return idx;
     }
 }
